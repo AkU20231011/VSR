64267e07ee7cefccf3144846289dd6b45094d591
Abhi <rds.abhisheku@gmail.com>
2023-07-21 12:06:43 +0530

    Added some new files and updated some files changed folder name

---
+++ JS/script.js
@@ -0,0 +1,67 @@
+jQuery(function($){
+$('.clients-slider').slick({
+slidesToShow: 2,
+slidesToScroll: 1,
+infinite:true,
+autoplay: false,
+autoplaySpeed: 700,
+arrows: false,
+dots: false,
+pauseOnHover: true,
+adaptiveHeight: true,
+prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+responsive: [{
+breakpoint: 980,
+settings: {
+slidesToShow: 1,
+slidesToScroll: 1
+}
+},
+{
+breakpoint: 480,
+settings: {
+slidesToShow: 1,
+slidesToScroll: 1
+}
+}
+]
+});
+});
+
+/* second slider*/
+
+jQuery(function($){
+    $('.img-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite:true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: false,
+    pauseOnHover: true,
+    adaptiveHeight: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [{
+    breakpoint: 980,
+    settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    }
+    },
+    {
+    breakpoint: 480,
+    settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    }
+    }
+    ]
+    });
+    });
